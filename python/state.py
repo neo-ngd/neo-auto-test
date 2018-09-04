e269a53594b01422044237f60994440a74084cc3
zhangtao <ztgoforit@163.com>
2018-09-04 17:34:23 +0800

    ouput check

--- python/state.py
+++ python/state.py
@@ -4,4 +4,6 @@ def getRss():
     state, output = commands.getstatusoutput('ps -ef -o pid -o rss -o size -o vsize -o sz -o comm | grep neo-cli | awk \'{print $2}\'')
     if state != 0:
         return -1
+    if output == '':
+        return -1
     return int(output)
